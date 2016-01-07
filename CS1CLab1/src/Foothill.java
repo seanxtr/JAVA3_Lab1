2dba19d36487a6aed4af39adf95f27a8a15707b9
Sheila Xu <Sheila Xu@s>
2016-01-07 14:47:01 -0800

    c

---
+++ CS1CLab1/src/Foothill.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+//------------------------------------------------------
+public class Foothill
+{
+	// -------  main --------------
+	public static void main(String[] args) throws Exception
+	{
+		int target = 72;
+		ArrayList<Integer> dataSet = new ArrayList<Integer>();
+		ArrayList<Sublist> choices = new ArrayList<Sublist>();
+		int k, j, numSets, max, kBest, masterSum;
+		boolean foundPerfect;
+
+		dataSet.add(2); dataSet.add(12); dataSet.add(22);
+		dataSet.add(5); dataSet.add(15); dataSet.add(25);
+		dataSet.add(9); dataSet.add(19); dataSet.add(29);
+
+		// code supplied by student
+
+    choices.get(kBest).showSublist();
+    }
+}
